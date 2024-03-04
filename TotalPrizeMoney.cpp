3e9b1df1b349be1cb622d1a8c6103094d087131f
Pabitra Kumar Sahoo <112860906+Pabitra-33@users.noreply.github.com>
2024-03-04 11:50:14 +0530

    first commit

---
+++ TotalPrizeMoney.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+    cin >> T;
+    int X,Y;
+    while(T--){
+        cin >> X;
+        cin >> Y;
+        cout << (X*10)+(Y*90)<<"\n";
+    }
+
+}
